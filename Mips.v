cf3c0e561f1c2165718e0b15ab3208fc706cdc19
muhammadnash <m.nashaat3@gmail.com>
2015-12-16 22:41:47 +0200

    Main Module

---
+++ Mips.v
@@ -0,0 +1,18 @@
+module MIPS();
+/*
+[CSE311 Final Project]
+[Main Module]
+	
+#1 ) Single cycle Implementation: (Lecture 1 > slide #20)
+=========================================================
+	Instruction Set Supported:
+	==========================
+		• Arithmetic: add, addi
+		• Load/Store: lw, sw
+		• Logic: sll, and, andi, nor
+		• Control flow: beq, jal, jr
+		• Comparison: slt
+
+*/
+
+endmodule
