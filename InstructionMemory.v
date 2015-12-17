3978ba678e5afec9bd6e76c8d26836f1e0b4497f
muhammadnash <m.nashaat3@gmail.com>
2015-12-17 21:57:30 +0200

    Creating instruction memory module

---
+++ InstructionMemory.v
@@ -0,0 +1,12 @@
+module InstructionMemory(instruction,address,clock);
+	input address[31:0],clock;
+	output instruction[31:0];
+	
+	reg [31:0]instruction;
+	reg [31:0]inst_memory[0:63]; // 64 instruction memory
+	
+	always @(posedge clock)
+		begin
+			instruction=inst_memory[address];
+		end
+endmodule 						  
