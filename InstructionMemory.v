52923ee29d44635b60b235af2d4d78449c8c146b
muhammadnash <m.nashaat3@gmail.com>
2015-12-17 22:25:35 +0200

    ++ reading instructions from file

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,10 +1,14 @@
 module InstructionMemory(instruction,address,clock);
-	input address[31:0],clock;
+	input address[5:0],clock;
 	output instruction[31:0];
 	
 	reg [31:0]instruction;
-	reg [31:0]inst_memory[0:63]; // 64 instruction memory
-	
+	reg [31:0]inst_memory[0:63]; //instruction memory of size 64 instruction , 32 bit each 
+								// 6 bits for addressing
+	initial 
+		begin
+			$readmemb("instruction_memory.list",inst_memory);
+		end 
 	always @(posedge clock)
 		begin
 			instruction=inst_memory[address];
