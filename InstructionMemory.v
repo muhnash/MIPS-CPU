3a7f3c310afd1994c0c26b3be0ede07f0d854be1
muhammadnash <m.nashaat3@gmail.com>
2015-12-18 15:18:22 +0200

    FIX : instruction_memory is now byte addressable , 256 byte ==> 64 instruction

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,16 +1,19 @@
 module InstructionMemory(instruction,address,clock);
-	input address[5:0],clock;
+	// byte addressable memory , 256 bytes , 64 instructions
+	input address[7:0],clock;  // 8 bits for adressing
 	output instruction[31:0];
 	
 	reg [31:0]instruction;
-	reg [31:0]inst_memory[0:63]; //instruction memory of size 64 instruction , 32 bit each 
-								// 6 bits for addressing
+	reg [7:0]inst_memory[0:255]; //instruction memory of size 256 byte, 64 instruction
 	initial 
 		begin
 			$readmemb("instruction_memory.list",inst_memory);
 		end 
 	always @(posedge clock)
 		begin
-			instruction=inst_memory[address];
+			instruction[31:24]=inst_memory[address];
+			instruction[23:16]=inst_memory[address+2'b01];
+			instruction[15:8]=inst_memory[address+2'b10];
+			instruction[7:0]=inst_memory[address+2'b11];
 		end
 endmodule 						  
