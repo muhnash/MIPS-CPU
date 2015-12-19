b37ae869a00590f1cd7654eaceb29ec96e586c42
unknown <mahmoudahmedrizk@gmail.com>
2015-12-19 21:59:09 +0200

    FINAL ALU module

--- ALU.v
+++ ALU.v
@@ -1,8 +1,20 @@
-module adde(in1,in2,control_in,out);
+module ALU(in1,in2,control_in,out,ZERO);
+	//in1: Rs input
+	//in2: Rt input
+	//control_in: 5-bit to choose between the ALU operations
+	//out: Rd or Rt output
+	//ZERO: is equal one when it's true
+	
+	//******************************************************
+	//ALL ALU operations are buit-in it EXCEPT Sign_Extender
+	//******************************************************
+	
+	
+	
 	input in1[31:0],in2[31:0],control_in[3:0];
-	output out[31:0];
-	reg [31:0]out;
-	wire w1;
+	output out[31:0],ZERO;
+	reg [31:0]out,ZERO;
+	
 	
 	always @(in1 or in2 or control_in)
 		begin 
@@ -42,7 +54,24 @@ module adde(in1,in2,control_in,out);
 				
 				out=~(in1 | in2);
 			end
-	
+			
+			if(control_in==4'b1000)begin //  beq  OPERATION
+				if(in1-in2==0) ZERO=1;
+				else ZERO=0;			  
+			end
+			
+			if(control_in==4'b1001)begin //   jal  OPERATION
+				
+					//DO NOTHING
+			end
+			if(control_in==4'b1010)begin //   jr   OPERATION
+					//DO NOTHING
+				
+			end
+			if(control_in==4'b1011)begin //   slt  OPERATION
+				if(in1<in2) out=1;
+				else out=0;
+			end
 	
 	
 		end
