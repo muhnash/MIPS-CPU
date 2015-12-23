58f4fec5070e2829b6a75fc9722f602980ce10e8
muhammadnash <m.nashaat3@gmail.com>
2015-12-23 08:07:00 +0200

    added opcodes for supported instructions

--- ControlUnit.v
+++ ControlUnit.v
@@ -9,33 +9,118 @@ module ControlUnit(RegDst,
 				   RegWrite,
 				   op_code);
 	
-	input  op_code[5:0];
-	output RegDst,
-		   Jump,
-		   Branch,
-		   MemRead,
-		   MemrtoReg,
-	       [1:0]ALUop,
-		   MemWrite,
-		   ALUsrc,
-		   RegWrite;
+	input  op_code[5:0]; 
+	output RegDst, Jump, Branch, MemRead, MemrtoReg, ALUop, MemWrite, ALUsrc,RegWrite;
 		   
-	reg    RegDst,
-		   Jump,  
-		   Branch,
-		   MemRead,
-		   MemrtoReg,
-	       ALUop,
-		   MemWrite,
-		   ALUsrc,
-		   RegWrite;
+	reg RegDst, Jump, Branch, MemRead, MemrtoReg,MemWrite, ALUsrc,RegWrite;   
+	reg [1:0]ALUop;
 	
 	always @ (op_code)
-		begin		  
+		begin	
 			case (op_code)
-				6'b000_000 : assign {out} = 9'b100_100_010; // R-type instructions
-				6'b100_011 : assign {out} = 9'b011_110_000; // lw instruction
-				6'b101_011 : assign {out} = 9'bx1x_001_000; // sw instruction
+				6'b000000 : begin
+								RegDst<=1'b1;	  // R-Format instructions [add , and , nor, jr,slt,sll]
+		   						Jump<=1'b0;
+		   						Branch<=1'b0;
+		   						MemRead<=1'b0;
+		   						MemrtoReg<=1'b0;
+	       						ALUop<=2'b10;
+		   						MemWrite<=1'b0;
+		   						ALUsrc<=1'b0;
+		   						RegWrite<=1'b1;
+							end
+					 
+				6'b001000 : begin
+								RegDst<=1'b0;	  // Addi (I format)
+		   						Jump<=1'b0;
+		   						Branch<=1'b0;
+		   						MemRead<=1'b0;
+		   						MemrtoReg<=1'b0;
+	       						ALUop<=2'b00;
+		   						MemWrite<=1'b0;
+		   						ALUsrc<=1'b0;
+		   						RegWrite<=1'b0;
+							end
+				
+				6'b100011 : begin
+								RegDst<=1'b0;	  // lw (I format)
+		   						Jump<=1'b0;
+		   						Branch<=1'b0;
+		   						MemRead<=1'b1;
+		   						MemrtoReg<=1'b1;
+	       						ALUop<=2'b00;
+		   						MemWrite<=1'b0;
+		   						ALUsrc<=1'b1;
+		   						RegWrite<=1'b1;
+							end
+					
+			   	6'b101011 : begin	
+				   				RegDst<=;	  // sw (I format)
+		   						Jump<=;
+		   						Branch<=;
+		   						MemRead<=;
+		   						MemrtoReg<=;
+	       						ALUop<=;
+		   						MemWrite<=;
+		   						ALUsrc<=;
+		   						RegWrite<=;
+				   			end
+				   
+				6'b001100 : begin	
+								RegDst<=;	  // andi (I format)
+		   						Jump<=;
+		   						Branch<=;
+		   						MemRead<=;
+		   						MemrtoReg<=;
+	       						ALUop<=;
+		   						MemWrite<=;
+		   						ALUsrc<=;
+		   						RegWrite<=;
+							end
+				
+				6'b000100 : begin
+								RegDst<=;	  // beq (I format)
+		   						Jump<=;
+		   						Branch<=;
+		   						MemRead<=;
+		   						MemrtoReg<=;
+	       						ALUop<=;
+		   						MemWrite<=;
+		   						ALUsrc<=;
+		   						RegWrite<=;
+							end
+				
+				6'b000011 : begin
+								RegDst<=;	  // jal (J format)
+		   						Jump<=;
+		   						Branch<=;
+		   						MemRead<=;
+		   						MemrtoReg<=;
+	       						ALUop<=;
+		   						MemWrite<=;
+		   						ALUsrc<=;
+		   						RegWrite<=;
+							end 
 			endcase
+			
 		end
 endmodule
+
+/* 	References
+	==========
+	# For Control signals or more datapath details ---> Textbook : Computer organization and design 5th ed 
+	
+	# For instructions OPCODE/FUNC/FORMAT
+		WIKIPERIA ---> https://www.wikiwand.com/en/MIPS_instruction_set
+	-----------------------------------------------------------------
+	add , and , nor , jr , slt , sll  [R-Format] i.e OPCODE=0
+	[opcodes in base 16] 
+	addi   	8	
+	lw 	 	 23
+	sw	  2B
+	andi  B
+	beq	   4
+	jal		3
+	
+*/	
+		
