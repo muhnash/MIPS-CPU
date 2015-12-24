57ca9d201094c771170a5cbf94e74d6febe09bc0
muhammadnash <m.nashaat3@gmail.com>
2015-12-24 10:07:33 +0200

    commiting what had to be committed (i thought it was :v ) yesterday

--- Mips.v
+++ Mips.v
@@ -13,6 +13,32 @@ module MIPS();
 		• Control flow: beq, jal, jr
 		• Comparison: slt
 
-*/
+*/	   
+
+
+// wires declarations
+wire clock;
+wire address,forced_instruction;  // for program counter 
+wire instruction;   // for instruction memory														  
+wire opcode, Rs, Rt, Rd ,shmt, func, immediate, target;// instruction decoder
+wire read_data1,read_data2, RegWrite,read_reg1, read_reg2, write_reg, write_data; // for register file
+wire RegDst, Jump , Branch, MemRead, MemrtoReg, ALUop, MemWrite, ALUsrc; // control unit
+wire in1,in2,control_in,out,ZERO; // ALU 
+ 
+	
+// Modules instantiation 
+ProgramCounter(address,forced_instruction, clock);
+
+InstructionMemory(instruction,address,clock);
+
+InstructionDecoder(opcode, Rs, Rt, Rd ,shmt, func, immediate, target, instruction);
+
+RegisterFile(read_data1,read_data2, RegWrite,read_reg1, read_reg2, write_reg, write_data, clock);
+
+ControlUnit(RegDst, Jump , Branch, MemRead, MemrtoReg, ALUop, MemWrite, ALUsrc, RegWrite, op_code);
+
+ALU(in1,in2,control_in,out,ZERO); 
+
+
 
 endmodule
