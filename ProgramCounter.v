3152d2a32f1243efde7a92997b49f8cdc66457b1
muhammadnash <m.nashaat3@gmail.com>
2015-12-25 09:00:23 +0200

    32 bit addressing for now

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -1,17 +1,20 @@
 module ProgramCounter(inst_address,next_inst_address, clock); 
-	// for insttruction memory of size 64 , 8 bit address
-	input next_inst_address[7:0], clock;
-	output  inst_address;
-	reg [7:0]inst_address;
+	// for instruction memory of size 64 , 8 bit address
+	// UPDATE : although the instruction mem size is just 64 bytes
+	// i'm addressing it with 32 bit address to be consisitent with the datapath shown in the Ref. P271
+	// also for avoiding more modifications at least for now , till it's up and running 
+	input [31:0]next_inst_address, clock;
+	output  [31:0]inst_address;
+	reg [31:0]inst_address;
 
 	initial 
 	begin
-		inst_address=8'b00000000;
+		inst_address=32'b00000000000000000000000000000000;  // initialy point to the 1st instruction i'e address 0
 	end
 
 	always @(posedge clock) 
 	begin
-		inst_address=next_inst_address;
+		inst_address=next_inst_address;  // updating the PC
 	end
 endmodule
 
