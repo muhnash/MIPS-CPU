ea9084b1b91cfedf51f833109507e9fe6a902fcc
unknown <mahmoudahmedrizk@gmail.com>
2015-12-26 00:57:19 +0200

    JR is not supported is this version

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,97 +1,111 @@
 module ControlUnit(RegDst, Jump , Branch, MemRead, MemrtoReg, ALUop, MemWrite, ALUsrc, RegWrite, op_code);
+	//###########################################################
+	//CONTROL_UNIT
+	//BASED on mips control table "See the Word file at source code directory" 
+	
+	//JR is	NOT SUPPORTED
+	
+	//###########################################################
+	
+	
 	
 	input  op_code[5:0]; 
-	output RegDst, Jump, Branch, MemRead, MemrtoReg, ALUop, MemWrite, ALUsrc,RegWrite;
-		   
-	reg RegDst, Jump, Branch, MemRead, MemrtoReg,MemWrite, ALUsrc,RegWrite;   
+	output RegDst[1:0], Jump[1:0], Branch, MemRead, MemrtoReg[1:0], ALUop[1:0], MemWrite, ALUsrc,RegWrite;
+	reg [1:0]RegDst;
+	reg [1:0]Jump;
+	reg [1:0]MemrtoReg;
 	reg [1:0]ALUop;
+	reg Branch,MemRead,MemWrite,ALUsrc,RegWrite;
 	
 	always @ (op_code)
 		begin	
-			case (op_code)
+			case (op_code)  
 				6'b000000 : begin
-						RegDst<=1'b1;	  // R-Format instructions [add , and , nor, jr,slt,sll]
-		   				Jump<=1'b0;
+						RegDst<=2'b01;	  // R-Format instructions [add , and , nor, jr,slt,sll]
+		   				Jump<=2'b00;
 		   				Branch<=1'b0;
 		   				MemRead<=1'b0;
-		   				MemrtoReg<=1'b0;
-	       				ALUop<=2'b100;
+		   				MemrtoReg<=2'b00;
+	       				ALUop<=2'b10;
    						MemWrite<=1'b0;
 		   				ALUsrc<=1'b0;
 		   				RegWrite<=1'b1;
 					    end
 					 
 				6'b001000 : begin
-						RegDst<=1'b0;	  // Addi (I format)
-		   				Jump<=1'b0;
+						RegDst<=2'b00;	  // Addi (I format)
+		   				Jump<=2'b00;
 						Branch<=1'b0;
 	  					MemRead<=1'b0;
-		   				MemrtoReg<=1'b0;
-	       				ALUop<=2'b00;
+		   				MemrtoReg<=2'b00;
+	       				ALUop<=2'b10;
 		   				MemWrite<=1'b0;
-		   				ALUsrc<=1'b0;
-		   				RegWrite<=1'b0;
+		   				ALUsrc<=1'b1;
+		   				RegWrite<=1'b1;
 		       			    end
 				
 				6'b100011 : begin
-						RegDst<=1'b0;	  // lw (I format)
-		   				Jump<=1'b0;
+						RegDst<=2'b00;	  // lw (I format)
+		   				Jump<=2'b00;
 		   				Branch<=1'b0;
 		   				MemRead<=1'b1;
-		   				MemrtoReg<=1'b1;
+		   				MemrtoReg<=2'b01;
 	       				ALUop<=2'b00;
 		   				MemWrite<=1'b0;
 		   				ALUsrc<=1'b1;
 		   				RegWrite<=1'b1;
 					    end
 					
-			   	6'b101011 : begin	
-				   		RegDst<=;	  // sw (I format)
-		   				Jump<=;
-		   				Branch<=;
-		   				MemRead<=;
-		   				MemrtoReg<=;
-	       				ALUop<=;
-		   				MemWrite<=;
-						ALUsrc<=;
-		   				RegWrite<=;
+	 		   	6'b101011 : begin	
+				   		RegDst<=2'bxx;	  // sw (I format)
+		   				Jump<=2'b00;
+		   				Branch<=1'b0;
+		   				MemRead<=1'b0;
+		   				MemrtoReg<=2'bxx;
+	       				ALUop<=2'b00;
+		   				MemWrite<=1'b1;
+		   				ALUsrc<=1'b1;
+		   				RegWrite<=1'b0;
 		   			    end
 				   
 				6'b001100 : begin	
-						RegDst<=;	  // andi (I format)
-		   				Jump<=;
-		   				Branch<=;
-		   				MemRead<=;
-		   				MemrtoReg<=;
-	       				ALUop<=;
-		   				MemWrite<=;
-		   				ALUsrc<=;
-		   				RegWrite<=;
+						RegDst<=2'b00;	  // andi (I format)
+		   				Jump<=2'b00;
+		   				Branch<=1'b0;
+		   				MemRead<=1'b0;
+		   				MemrtoReg<=2'b00;
+	       				ALUop<=2'b10;
+		   				MemWrite<=1'b0;
+		   				ALUsrc<=1'b1;
+		   				RegWrite<=1'b1;
 				  	    end
 				
 				6'b000100 : begin
-						RegDst<=;	  // beq (I format)
-		   				Jump<=;
-		   				Branch<=;
-		   				MemRead<=;
-		   				MemrtoReg<=;
-	       					ALUop<=;
-		   				MemWrite<=;
-		   				ALUsrc<=;
-		   				RegWrite<=;
+						RegDst<=2'bxx;	  // beq (I format)
+		   				Jump<=2'b00;
+		   				Branch<=1'b1;
+		   				MemRead<=1'b0;
+		   				MemrtoReg<=2'bxx;
+	       				ALUop<=2'b01;
+		   				MemWrite<=1'b0;
+		   				ALUsrc<=1'b0;
+		   				RegWrite<=1'b0;
 				    	    end
 				
 				6'b000011 : begin
-						RegDst<=;	  // jal (J format)
-		   				Jump<=;
-		   				Branch<=;
-		   				MemRead<=;
-		   				MemrtoReg<=;
-	       					ALUop<=;
-		   				MemWrite<=;
-		   				ALUsrc<=;
-		   				RegWrite<=;
-					    end 
+						RegDst<=2'b10;	  // jal (J format)
+		   				Jump<=2'b01;
+		   				Branch<=1'bx;
+		   				MemRead<=1'b0;
+		   				MemrtoReg<=2'b10;
+	       				ALUop<=2'bxx;
+		   				MemWrite<=1'b0;
+		   				ALUsrc<=1'bx;
+		   				RegWrite<=1'b1;
+				  			end
+				
+							  
+							  
 			endcase
 			
 		end
