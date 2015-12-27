920c9f21576970154c4874fbcabe8a52de9caf30
unknown <mahmoudahmedrizk@gmail.com>
2015-12-27 13:44:14 +0200

    nashaat was here

--- MUX_2x1.v
+++ MUX_2x1.v
@@ -5,8 +5,11 @@ input [31:0]value1;
 input [31:0]value2;
 output [31:0]out; 
 reg [31:0]out;
+
+
+
 	
-always @(sel or value1 or value2 )
+always @(sel or value1 or value2 )	
 	begin
 	if(sel==1'b0)  out=value1;			   
 	else if(sel==1'b1) out=value2;
